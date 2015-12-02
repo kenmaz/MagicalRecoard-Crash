a5fc2c097955f4997626f1f51591401c826932f8
Kentaro Matsumae <kentaro.matsumae@dena.com>
2015-12-02 21:19:08 +0900

    Initial Commit

---
+++ MRCrash/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MRCrash
+//
+//  Created by Kentaro Matsumae on 2015/12/02.
+//  Copyright © 2015年 kenmaz.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MRCrash/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MRCrash
+//
+//  Created by Kentaro Matsumae on 2015/12/02.
+//  Copyright © 2015年 kenmaz.net. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
